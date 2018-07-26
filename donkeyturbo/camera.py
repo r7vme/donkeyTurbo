b8aa6d23792384092e87c511573d2253652fb04c
Roman Sokolkov <roman@giantswarm.io>
2018-07-26 23:01:20 +0200

    Add SMA for angle computing and fix drive mode

--- donkeyturbo/camera.py
+++ donkeyturbo/camera.py
@@ -8,14 +8,14 @@ from donkeycar.parts.camera import PiCamera as DonkeyCamera
 
 # NOTE(r7vme): The only difference is hardcoded resolution and added zoom.
 class DTPiCamera(DonkeyCamera):
-    def __init__(self, resolution=(120, 160), framerate=20):
+    def __init__(self, resolution=(160, 78), framerate=20):
         from picamera.array import PiRGBArray
         from picamera import PiCamera
         # initialize the camera and stream
         self.camera = PiCamera() #PiCamera gets resolution (height, width)
 
         # Crop image from the top.
-        self.camera.resolution = (160, 78)
+        self.camera.resolution = resolution
         self.camera.zoom = (0.0, 0.35, 1.0, 1.0)
 
         self.camera.framerate = framerate
--- donkeyturbo/keras.py
+++ donkeyturbo/keras.py
@@ -3,14 +3,31 @@ Module contains pilot class and Keras model.
 '''
 
 import donkeycar as dk
+from collections import deque
 from donkeycar.parts.keras import KerasPilot
 
-
+# Main features:
+# - linear throttle computation based on angle.
+# - angle post computed with SMA.
 class DTKerasPilot(KerasPilot):
-    def __init__(self, *args, **kwargs):
-        super(DTKerasPilot, self).__init__(*args, **kwargs)
+    def __init__(self, config=None):
+        super(DTKerasPilot, self).__init__()
+
+        if config is None:
+            # Set default values.
+            config = {
+                    'angle_sma_n': 3,
+                    'throttle_max': 1.0,
+                    'throttle_min': 0.5,
+                    }
+
+        # TODO: do validation
+        self.config = config
 
         self.model = dt_categorical()
+
+        # Initialize fixed length FIFO queue for angles historical data.
+        self.angles = deque([], self.config['angle_sma_n'])
 
     def run(self, img_arr):
         # Do NN prediction.
@@ -20,22 +37,34 @@ class DTKerasPilot(KerasPilot):
 
         # Do post processing.
         angle = self.compute_angle(angle_unbinned)
-        throttle = self.compute_throttle(angle_unbinned)
+        throttle = self.compute_throttle(angle)
 
+        print('throttle:', throttle, 'angle:', angle)
         return angle, throttle
 
+    # _compute_sma computes SMA (simple moving average).
+    # https://en.wikipedia.org/wiki/Moving_average#Simple_moving_average
+    @staticmethod
+    def _compute_sma(data):
+        return sum(data) / len(data)
+
     def compute_angle(self, angle):
-        return angle
+        # Append new angle to historial data.
+        self.angles.append(angle)
+        # Return SMA value.
+        return self._compute_sma(self.angles)
 
     def compute_throttle(self, angle):
-        throttle = 1 - abs(angle)
-        # TODO(r7vme): Make this configurable.
-        if throttle <= 0.5:
-            throttle = 0.5
-        if throttle >= 0.6:
-            throttle = 0.6
-        print('throttle', throttle)
-        return throttle
+        b = self.config['throttle_min']
+        s = self.config['throttle_max'] - self.config['throttle_min']
+        x = 1 - abs(angle)
+
+        # b - base, minimally allowed speed
+        # s - scale, basically delta between max and min limits
+        # x - linear dependency from angle
+        th = b + s * x
+
+        return th
 
 # NOTE(r7vme): Just a copy of default_categorical with zoomed input layer.
 def dt_categorical():
